Changed convention for camera parameters, updated visibility computation, fixed robust loss mistake, updated plot

preprocessing/preprocessing.py
@@ -4,12 +4,14 @@ import scipy.optimize
 
 import open3d as o3d
 from PIL import Image
+import matplotlib.pyplot as plt
 
 import os.path
 import time
 
 from utils import *
 from robust_mean import loss
+import visualization
 
 import colmap.read_write_model
 
@@ -17,9 +19,8 @@ import colmap.read_write_model
 input_path = "../../../data/horse_workspace/"
 input_format = ".txt"
 
-cameras, images, points3D = colmap.read_write_model.read_model(path=input_path,
-                                                        ext=input_format)
-
+model_cameras, model_images, _ = \
+    colmap.read_write_model.read_model(path=input_path, ext=input_format)
 
 dense_cloud = o3d.io.read_point_cloud(input_path + "dense/0/fused.ply")
 dense_cloud.normalize_normals()
@@ -29,206 +30,181 @@ points = np.array(dense_cloud.points)
 normals = np.array(dense_cloud.normals)
 colors = np.array(dense_cloud.colors)
 
-# TODO: determine correct number of cameras from
-camera_dicts = np.array([get_camera_parameters(cameras[idx], images[idx]) for idx in range(1, 149)])
+# Extract poses and camera parameters from model variables
+poses = np.array([get_pose(model_images[key]) for key in model_images.keys()])
+camera_parameters = get_camera_parameters(model_cameras[1])
+intrinsics = camera_parameters['intrinsics'] 
+distortion = camera_parameters['distortion']
+width, height = camera_parameters['resolution']
 
-vis = o3d.visualization.Visualizer()
-vis.create_window(width=1920, height=1080, visible=False)
-vis.add_geometry(mesh)
-vis.poll_events()
-vis.update_renderer()
-
-images = [Image.open(input_path + "images/" + '0' * (5 - len(str(i))) + str(i) + ".jpg") for i in range(1, 150)]
-depth_images = []
-
-for camera in camera_dicts :
-    set_visualizer_camera_parameters(vis, camera)
-    depth_images.append(vis.capture_depth_float_buffer())
-
-
-x = np.arange(0, 1920, 1)
-y = np.arange(0, 1080, 1)
 
 recompute_visibility = False
 if os.path.isfile("visibility_matrix.npy") and not recompute_visibility :
     visibility_matrix = np.load("visibility_matrix.npy")
     print("Loaded visibility matrix")
 else :
-    visibility_matrix = np.zeros([len(camera_dicts), len(points)], dtype=bool)
-
+    visibility_matrix = np.zeros([len(points), len(poses)], dtype=bool)
+    # Render depth images from mesh
+    pinhole = get_pinhole_from_simple_radial(camera_parameters)
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(width=width, height=height, visible=False)
+    vis.add_geometry(mesh)
+    vis.poll_events()
+    vis.update_renderer()
+    set_visualizer_intrinsics(vis, pinhole['intrinsics'], pinhole['resolution'])
+    
+    depth_images = []
+    for pose in poses :
+        set_visualizer_pose(vis, pose)
+        depth_images.append(vis.capture_depth_float_buffer())
+    
     margin = 5
-
-    for i, camera in enumerate(camera_dicts) :
-        print("Camera " + str(i))
-        x_image, _, X_C = project_steps(points, camera, False)
-        point_selector = np.logical_and(
-                             np.logical_and(x_image[:, 0] >= 0+margin, x_image[:, 1] >= 0+margin),
-                             np.logical_and(x_image[:, 0] < 1920+margin, x_image[:, 1] < 1080+margin))
-        x_image = x_image[point_selector, :]
-        X_C = X_C[point_selector, :]
-        depth_image_bilin = interpolate.RectBivariateSpline(y, x, np.array(depth_images[i]), kx=1, ky=1)
-        ratio = depth_image_bilin(x_image[:, 1], x_image[:, 0], grid=False) / X_C[:, 2]
+    
+    for i, pose in enumerate(poses) :
+        if i % 10 == 0 :
+            print("Visibility for view " + str(i))
+        x_depth, a, X_C = project_steps(points, pose, pinhole['intrinsics'])
+        x_image, b, c, d = project_steps(points, pose, intrinsics, distortion)
         
+        visible_depth = np.logical_and(np.logical_and(x_depth[:, 0] >= 0, 
+                                                      x_depth[:, 1] >= 0),
+                                       np.logical_and(x_depth[:, 0] < width, 
+                                                      x_depth[:, 1] < height))
+        
+        visible_image = np.logical_and(np.logical_and(x_image[:, 0] >= 0 + margin, 
+                                                      x_image[:, 1] >= 0 + margin),
+                                       np.logical_and(x_image[:, 0] < width - margin, 
+                                                      x_image[:, 1] < height - margin))
+        
+        visible_selection = np.logical_and(visible_depth, visible_image)
+        
+        x_depth = x_depth[visible_selection, :]
+        x_image = x_image[visible_selection, :]
+        X_C =  X_C[visible_selection, :]
+        
+        depth_image_bilin = interpolate.RegularGridInterpolator((np.arange(0, height, 1),
+                                                                 np.arange(0, width, 1)), 
+                                                                np.array(depth_images[i]))
+        
+        ratio = depth_image_bilin(np.flip(x_depth, axis=1)) / X_C[:, 2]
         depth_selection = np.logical_and(ratio < 1.01, ratio > 0.99)
+
+        # Only select close points, optional
         depth_selection = np.logical_and(depth_selection, X_C[:, 2] < 4.)
         
-        point_selector[point_selector] = depth_selection
-        visibility_matrix[i, :] = point_selector
+        visible_selection[visible_selection] = depth_selection
+        
+        visibility_matrix[:, i] = visible_selection
 
     np.save("visibility_matrix", visibility_matrix)
-    
 
 
-
-visibility_matrix = visibility_matrix.T
-visible_points = visibility_matrix.sum(axis=1) > 2
-print(visible_points.sum())
+# Remove points that are not visible
+visible_points = visibility_matrix.sum(axis=1) > 0
 visibility_matrix = visibility_matrix[visible_points, :]
 points = points[visible_points, :]
 normals = normals[visible_points, :]
+colors = colors[visible_points, :]
 
 
-
-
-print("start")
-
-z = np.arange(0, len(images), 1)
+# Load images and create interpolation function
+images = [Image.open(input_path + "images/" + '0' * (5 - len(str(i))) + str(i) + ".jpg") for i in range(1, len(poses)+1)]
 image_stack = np.array([np.array(image) for image in images]).mean(axis=3)
-images_interp = interpolate.RegularGridInterpolator((z, y, x), image_stack)
+images_interp = interpolate.RegularGridInterpolator((np.arange(0, len(images), 1), 
+                                                     np.arange(0, height, 1), 
+                                                     np.arange(0, width, 1)), 
+                                                    image_stack,
+                                                    bounds_error=False)
 
 
 batch_size = 10000
-secs_all = time.time()
-secs = time.time()
 
-
-stacked_grid_spacing = True
-
-if stacked_grid_spacing :
-    scales, _, grid_outer = optimize_grid_spacing_multiple(points[:batch_size, :], normals[:batch_size, :], camera_dicts, visibility_matrix[:batch_size, :])
-    scales = scales / np.sqrt(2)
-else :
-    for idx in range(batch_size) :
-        if idx % 500 == 0 : 
-            print(idx)
-        visible_idx = np.argwhere(visibility_matrix[idx, :]).reshape(-1)
-        visible_cameras = [camera_dicts[i] for i in visible_idx]
-        
-        # optimize grid spacing
-        grid_scale, grid_directions, grid_outer = \
-            optimize_grid_spacing(points[[idx], :], normals[[idx], :], 
-                                  visible_cameras)
-
-
-print("finished optimize grid spacing")
-print(time.time() - secs)
-
-visibility_idx = np.argwhere(visibility_matrix[:batch_size, :])
-
-grids = create_grids(points[:batch_size], normals[:batch_size], scales)
-
-grids_I, _, _, _ = project_separate_steps(grids[visibility_idx[:, 0]].reshape(-1, 16, 3), camera_dicts[visibility_idx[:, 1]], True)
-
-
-
-# TODO: plot 
-
-
-
-
-
-grid_z = visibility_idx[:, [1]].repeat(16, axis=1)
-
-grids_I_interp_coord = np.concatenate([grid_z.reshape(-1, 1), 
-                                       np.flip(grids_I.reshape(-1, 2), axis=1)], axis=1)
-
-secs = time.time()
-patch_stack = images_interp(grids_I_interp_coord).reshape(-1, 16)
-
-patch_stack_un = patch_stack
-patch_stack_un_norm = np.linalg.norm(patch_stack_un, axis=1)
-print("Filtering")
-print(patch_stack_un_norm.min())
-
-
-# TODO: normalize patches
-mu = patch_stack.mean(axis=1).reshape(-1, 1)
-sigma = np.linalg.norm(patch_stack - mu, axis=1).reshape(-1, 1)
-
-patch_stack = (patch_stack - mu) / sigma
-
-# TODO: how to transfer this validity
-valid = (sigma!=0).reshape(-1)
-patch_stack = patch_stack[valid, :]
-
-
-visibility_matrix_local = visibility_matrix[:batch_size, :]
-visibility_idx = np.argwhere(visibility_matrix_local)
-visibility_matrix_local[visibility_idx[:, 0], visibility_idx[:, 1]] = (sigma != 0).reshape(-1)
-visibility_idx = np.argwhere(visibility_matrix_local)
-
-secs = time.time()
-#TODO: sparse matrix # TODO: list of 16 sparse matrices
-patch_fulls = [scipy.sparse.csr_matrix((patch_stack[:, i] , (visibility_idx[:, 0], visibility_idx[:, 1]))) for i in range(16)]
-print("patch_fulls")
-patch_mean = np.zeros([visibility_matrix_local.shape[0], 16])
-for i, patch_full in enumerate(patch_fulls) :
-    patch_mean[:, i] = (np.asarray(patch_full.sum(axis=1)).reshape(-1) / visibility_matrix_local.sum(axis=1))
-
-#patch_full = np.zeros([visibility_matrix_local.shape[0], len(camera_dicts), 16])
-#patch_full[:] = np.nan
-#patch_full[visibility_idx[:, 0], visibility_idx[:, 1], :] = patch_stack
-#patch_mean = np.nanmean(patch_full, axis=1)
-
-print(patch_mean.shape)
-
-print("success")
-print(time.time() - secs)
-
-stacked_optimization = True
-
-if stacked_optimization :
-# Robust Mean Calculation
-    row_idx = np.array(range(len(visibility_idx[:, 0]))).reshape(-1, 1).repeat(16, 1).reshape(-1)
-    col_idx = np.array(range(batch_size*16)).reshape(-1, 16)[visibility_idx[:, 0], :]
-    col_idx = col_idx.reshape(-1)
-
-    def f(x, patches, visibility_idx) :
-        residuals = np.linalg.norm(x.reshape(-1, 16)[visibility_idx[:, 0], :] - patches, axis=1)
-        return residuals
-    def jac(x, patches, visibility_idx) :
-        # TODO: sparse matrix
-        vals = x.reshape(-1,16)[visibility_idx[:, 0], :] / np.linalg.norm(x.reshape(-1, 16)[visibility_idx[:, 0], :] - patches, axis=1).reshape(-1, 1)
-        print(vals.shape)
-        j = scipy.sparse.csr_matrix((vals.reshape(-1), (row_idx, col_idx)))
-        return j
-        
-    # Matrix to indicate sparsity of the jacobian
-    jac_sparsity = scipy.sparse.csr_matrix((np.ones(len(visibility_idx[:, 0])*16), 
-                                           (row_idx, col_idx)))
+print(len(points))
+print(range(int(np.ceil(len(points) / batch_size))))
+for batch_idx in range(int(np.ceil(len(points) / batch_size))) :
+    batch_begin = batch_idx*batch_size
+    batch_end = (batch_idx+1)*batch_size
     secs = time.time()
-    result = scipy.optimize.least_squares(f, x0=patch_mean.reshape(-1), jac=jac,
-                                loss=loss, args=(patch_stack, visibility_idx), jac_sparsity=jac_sparsity,
-                                method='trf')
-else :
+    
+    scales, _, _ = optimize_grid_spacing_multiple(points[batch_begin:batch_end, :], 
+                                                  normals[batch_begin:batch_end, :], 
+                                                  poses,
+                                                  camera_parameters, 
+                                                  visibility_matrix[batch_begin:batch_end, :])
+    scales = scales / np.sqrt(2)
+    print("Optimize grid spacing: " + str(time.time() - secs))
+
+
+    visibility_idx = np.argwhere(visibility_matrix[batch_begin:batch_end, :])
+
+    grids = create_grids(points[:batch_size], normals[batch_begin:batch_end], scales)
+    grids_I, _, _, _ = project_separate_steps(grids[visibility_idx[:, 0]].reshape(-1, 16, 3), 
+                                              poses[visibility_idx[:, 1], :], intrinsics, distortion)
+
+
+    grid_z = visibility_idx[:, [1]].repeat(16, axis=1)
+    grids_I_interp_coord = np.concatenate([grid_z.reshape(-1, 1), 
+                                           np.flip(grids_I.reshape(-1, 2), axis=1)], axis=1)
+    patch_stack = images_interp(grids_I_interp_coord).reshape(-1, 16)
+
+
+    mu = patch_stack.mean(axis=1).reshape(-1, 1)
+    sigma = np.linalg.norm(patch_stack - mu, axis=1).reshape(-1, 1)
+    patch_stack = (patch_stack - mu) / sigma
+    
+    valid = (sigma!=0).reshape(-1)
+    patch_stack = patch_stack[valid, :]
+    grids_I = grids_I[valid, :]
+    
+    visibility_matrix_local = visibility_matrix[batch_begin:batch_end, :]
+    visibility_idx = np.argwhere(visibility_matrix_local)
+    visibility_matrix_local[visibility_idx[:, 0], visibility_idx[:, 1]] = (sigma != 0).reshape(-1)
+    visibility_idx = np.argwhere(visibility_matrix_local)
+    
+    patch_fulls = [scipy.sparse.csr_matrix((patch_stack[:, i] , (visibility_idx[:, 0], visibility_idx[:, 1]))) for i in range(16)]
+    
+    patch_mean = np.zeros([visibility_matrix_local.shape[0], 16])
+    patch_min = np.zeros([visibility_matrix_local.shape[0], 16])
+    patch_max = np.zeros([visibility_matrix_local.shape[0], 16])
+    
+    for i, patch_full in enumerate(patch_fulls) :
+        patch_mean[:, i] = (np.asarray(patch_full.sum(axis=1)).reshape(-1) / visibility_matrix_local.sum(axis=1))
+        patch_min[:, i] = patch_full.min(axis=1).todense().reshape(-1)
+        patch_max[:, i] = patch_full.max(axis=1).todense().reshape(-1)
+    
+    
+    patch_full = np.zeros([visibility_matrix_local.shape[0], len(poses), 16])
+    patch_full[:] = np.nan
+    patch_full[visibility_idx[:, 0], visibility_idx[:, 1], :] = patch_stack
+    patch_median = np.nanmedian(patch_full, axis=1)
+
+    robust_mean = np.zeros(patch_mean.shape)
+    
     def f(x, patch) :
-        return np.linalg.norm(x.reshape(-1, 16) - patch.reshape(-1, 16), axis=1)
+        residual = np.linalg.norm(x.reshape(-1, 16) - patch.reshape(-1, 16), axis=1)
+        return residual
     def jac(x, patch) :
-        return x.reshape(1, -1) / np.linalg.norm(x.reshape(-1, 16) - patch.reshape(-1, 16), axis=1).reshape(-1, 1)
+        j = (x.reshape(-1, 16) - patch.reshape(-1, 16)) / np.linalg.norm(x.reshape(-1, 16) - patch.reshape(-1, 16), axis=1).reshape(-1, 1)
+        return j
+    
     secs = time.time()
     for i in range(len(patch_mean)) :
-        if i % 500 == 0:
-            print(i)
-        result = scipy.optimize.least_squares(f, x0=patch_mean[i, :], jac=jac,
-                                loss=loss, args=(patch_stack[visibility_idx[i,0], :].reshape(1, -1, 16)),
-                                method='dogbox')
-        if i==0 :
-            print(result.x)
-print(result)
+        result = scipy.optimize.least_squares(f, x0=patch_mean[i, :], jac=jac, 
+                                loss=loss, args=(patch_stack[visibility_idx[:,0]==i, :].reshape(1, -1, 16)),
+                                method='dogbox') #, tr_solver='lsmr', verbose=0)
+        robust_mean[i] = result.x
+        
+    print("Optimize robust mean: " + str(time.time() - secs))
 
-print("finished optimize least squares")
-print(time.time() - secs)
-robust_mean = result.x.reshape(-1, 16)
-print(time.time() - secs_all)
-print("End")
+
+    plot_idx = 10
+    for plot_idx in [0, 10, 20, 30, 45] :
+        visualization.plot_grids(grids_I[visibility_idx[:, 0] == plot_idx, :, :],
+                                 visibility_idx[visibility_idx[:, 0] == plot_idx, 1], 
+                                 np.array([np.array(image) for image in images]))
+        visualization.plot_patches(patch_stack[visibility_idx[:, 0] == plot_idx, :], 
+                                   robust_mean[plot_idx, :], patch_median[plot_idx, :], patch_mean[plot_idx, :], 
+                                   visibility_idx[visibility_idx[:, 0] == plot_idx, 1])
+        plt.show()
+    
 

preprocessing/visualization.py
@@ -1,12 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_grids(grid_I, visibility_idx, images) :
+    # TODO
+    grid_centers = grid_I.mean(axis=1)
+
+    plot_height = int(np.sqrt(len(grid_I)))
+    plot_width = int(np.ceil(len(grid_I) / plot_height))
+
+    fig = plt.figure()
+    current_plot = 1
+    area = 40
+    for i, idx in enumerate(visibility_idx) :
+        lower = grid_centers[i,:].astype(int) - area
+        higher = grid_centers[i, :].astype(int) + area
+        higher = higher - np.minimum(lower, 0)
+        bounds = np.array([np.array(images[idx]).shape[1], np.array(images[idx]).shape[0]])
+        lower = lower + np.maximum(higher, bounds) - bounds
+        lower = np.maximum(lower, 0)
+        higher = np.minimum(higher, bounds)
+        
+        ax = fig.add_subplot(plot_height, plot_width, current_plot)
+        ax.imshow(np.array(images[idx])[lower[1]:higher[1], lower[0]:higher[0]], interpolation="bilinear")
+        # TODO: 
+        ax.scatter(grid_I[i, :, 0] - lower[0], grid_I[i, :, 1] - lower[1],
+                   facecolors='none', edgecolors='r', linewidth=0.5, s=2)
+        # TODO: change title
+        ax.set_title("View " + str(idx), fontsize=8)
+        ax.set_axis_off()
+        current_plot += 1
+        #ax.set_yticklabels([]); ax.set_xticklabels([])
+    #plt.show()
 
 
 
-def plot_patches(patches_stack, robust_mean, mean, closest) :
-    num_images = len(patches_stack) + 3
+def plot_patches(patches_stack, robust_mean, median, mean, view_idxs=None) :
+    # TODO
+    num_images = len(patches_stack)
     vmax = 0.6
     
-    plot_height = int(np.sqrt(num_images))
-    plot_width = int(np.ceil(num_images / plot_height))
+    closest = np.argmin(np.linalg.norm(patches_stack - robust_mean.reshape(-1, 16), axis=1))
+    # TODO: find closest
+    
+    plot_height = int(max(2, np.sqrt(num_images) + 1)) + 1
+    plot_width = int(max(4, np.ceil(num_images / plot_height)))
     current_plot = 1
     
     fig = plt.figure()
@@ -17,23 +54,29 @@ def plot_patches(patches_stack, robust_mean, mean, closest) :
     ax.set_title("Robust Mean", fontsize=8)
     current_plot += 1
     ax = fig.add_subplot(plot_height, plot_width, current_plot)
+    ax.imshow(median.reshape(4,4), vmin=-vmax, vmax=vmax)
+    ax.set_axis_off()
+    ax.set_title("Median", fontsize=8)
+    current_plot += 1
+    ax = fig.add_subplot(plot_height, plot_width, current_plot)
     ax.imshow(mean.reshape(4,4), vmin=-vmax, vmax=vmax)
     ax.set_axis_off()
     ax.set_title("Mean", fontsize=8)
     current_plot += 1
     ax = fig.add_subplot(plot_height, plot_width, current_plot)
-    ax.imshow(closest.reshape(4,4), vmin=-vmax, vmax=vmax)
+    ax.imshow(patches_stack[closest, :].reshape(4,4), vmin=-vmax, vmax=vmax)
     ax.set_axis_off()
     ax.set_title("Closest", fontsize=8)
     current_plot += 1
-    
+    current_plot = plot_width+1
     for i in range(len(patches_stack)) :
         ax = fig.add_subplot(plot_height, plot_width, current_plot)
         ax.imshow(patches_stack[i, :].reshape(4,4), vmin=-vmax, vmax=vmax)
         ax.set_axis_off()
+        ax.set_title("View " + str(view_idxs[i]), fontsize=8)
         current_plot += 1
     
-    plt.show()
+    #plt.show()
 
 
 def combined_plot(images, patches_stack, robust_mean, mean, closest) :