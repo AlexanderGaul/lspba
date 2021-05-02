1100d42bbcb713392b30b10e7fa90c697745dfd6
Alexander Gaul <gaul.alexander@googlemail.com>
2021-05-02 16:31:18 +0200

    Optimization: Refactor data perturbation and image loading into separate function

--- optimization/lm.cpp
+++ optimization/lm.cpp
@@ -248,90 +248,164 @@ int main(int argc, char **argv) {
         if (i % modulo_selection == 0)
             selection.push_back(i);
     }
+
     std::cout << "Landmarks: " << pixels.size() << std::endl;
     std::cout << "Observations: " << observations << std::endl;
     std::cout << "Selection: " << selection.size() << std::endl;
     
     std::vector<cv::Mat> images_color;
+    load_images(image_path, images_color);
     
-    std::vector<std::array<cv::Mat, 3>> mat_pyramids;
+    std::vector<std::vector<cv::Mat>> mat_pyramids;
     std::vector<std::vector<ceres::Grid2D<double, 1>>> grid_pyramids;
     std::vector<std::vector<ceres::BiCubicInterpolator<ceres::Grid2D<double, 1>>>> interpolator_pyramids;
-    /* TODO: reserve space in vectors */
-    // Read images
-    int image_name_length = std::filesystem::directory_iterator(image_path)->path().filename().string().size() - 4;
-    std::string extension = std::filesystem::directory_iterator(image_path)->path().filename().extension().string();
-    for (int i = 0; i < poses.size(); i++) {
-        cv::Mat mat1;
-        cv::Mat mat2;
-        std::string im_file = image_path + (std::string((image_name_length - std::to_string(i+1).length()), '0') + std::to_string(i+1) + extension);
-        
-        images_color.push_back(cv::imread(im_file));
-        
-        // Create image pyramids with OpenCV
-        cv::imread(im_file, cv::IMREAD_GRAYSCALE).convertTo(mat1, CV_64F);
-        // TODO: test this
-        if (true) {
-            cv::pyrDown(mat1, mat2); //cv::BORDER_REPLICATE
-        }
-        mat1 /= 255.;
-        mat2 /= 255.;
-        mat_pyramids.push_back({mat1, mat2});
+    std::vector<std::vector<basalt::Image<double>>> basalt_pyramids;
+    
+    create_pyramid_images(images_color,
+                          mat_pyramids,
+                          &grid_pyramids,
+                          &interpolator_pyramids,
+                          &basalt_pyramids);
+    
+    
+    ProblemParameters param_all(planes, poses, camera_param, pixels, 
+                                 source_views, visibilities, interpolator_pyramids, basalt_pyramids,
+                                 images_color, mat_pyramids);
+    //param_all.filter_extreme_angles();
+    
+    ProblemParameters param_initial(param_all, selection);
+    
+    double max_x = 0.;
+    double max_y = 0.;
+    for (int i = 0; i < param_initial.planes.size(); i++) {
+        if (param_initial.get_point(i)[0] > max_x) max_x = param_initial.get_point(i)[0];
+        if (param_initial.get_point(i)[1] > max_y) max_y = param_initial.get_point(i)[1];
     }
-    for (int i = 0; i < poses.size(); i++) {
-        grid_pyramids.push_back({ceres::Grid2D<double, 1>((double*)(mat_pyramids[i][0].data), 0, mat_pyramids[i][0].rows, 0, mat_pyramids[i][0].cols),
-                                 ceres::Grid2D<double, 1>((double*)(mat_pyramids[i][1].data), 0, mat_pyramids[i][1].rows, 0, mat_pyramids[i][1].cols)});
+    std::cout << "-------" << std::endl;
+    std::cout << max_x << std::endl;
+    std::cout << max_y << std::endl;
+    
+    std::vector<long> selection_center;
+    for (int i = 0; i < param_all.pixels.size(); i++) {
+        Eigen::Vector3d p = param_all.get_point(i);
+        if (p[0] < 0.075 && p[0] > -0.075 && p[1] < 0.075 && p[1] > -0.075)
+            selection_center.push_back(i);
     }
-    for (int i = 0; i < poses.size(); i++) {
-        interpolator_pyramids.push_back({ceres::BiCubicInterpolator<ceres::Grid2D<double, 1>>(grid_pyramids[i][0]),
-                                         ceres::BiCubicInterpolator<ceres::Grid2D<double, 1>>(grid_pyramids[i][1])});
-    }
     
-    problem_parameters param_all(planes, poses, camera_param, pixels, source_views, visibilities, interpolator_pyramids);
+    //param_initial.filter_extreme_angles();
+    ProblemParameters param_center(param_all, selection_center);
+    ProblemParameters param_initial_perturbed = perturb_data(param_initial);
     
-    problem_parameters param_initial(param_all, selection);
-    problem_parameters param_initial_perturbed{param_initial};
-    
-    RadialCamera<double> camera{camera_param};
-    
-    std::vector<Eigen::Vector3d> p3D = param_initial_perturbed.get_points();
-    std::vector<Eigen::Vector3d> normals_init = param_initial_perturbed.get_normals();
-    std::vector<Eigen::Vector3d> planes_perturbed;
-    
-    std::default_random_engine generator;
-    std::normal_distribution<double> distribution(0.0, 0.005);
-    for (int i = 0; i < p3D.size(); i++) {
-        Eigen::Vector3d p_C = poses[param_initial_perturbed.source_views[i]] * p3D[i];
-        double perturbation = -100.;
-        while (abs(perturbation) > 0.01) {
-            perturbation = distribution(generator);
-        }
-        p_C = p_C * ((p_C[2] + perturbation) / p_C[2]);
-        planes_perturbed.push_back(
-                    get_plane(p_C, 
-                              poses[param_initial_perturbed.source_views[i]].rotationMatrix() * normals_init[i]));
-    }
-    param_initial_perturbed.planes = planes_perturbed;
-    
-    problem_parameters param_optimize{param_initial};
-    problem_parameters param_optimize_perturbed{param_initial_perturbed};
 
-    solve_problem(param_optimize, 1);
-    solve_problem(param_optimize, 0);
     
-    solve_problem(param_optimize_perturbed, 1);
-    solve_problem(param_optimize_perturbed, 0);
+    std::cout << param_initial.get_normal(0).transpose() << std::endl;
+    std::cout << param_initial_perturbed.get_normal(0).transpose() << std::endl;
+    std::cout << "Perturbation comparison" << std::endl;
+    std::cout << param_initial_perturbed.get_normals()[0].transpose() << std::endl;
+    std::cout << z_error(param_initial, -0.25) << std::endl;
+    std::cout << z_error(param_initial_perturbed, -0.25) << std::endl;
+    std::cout << normal_error(param_initial, {0., 0., 1.}) << std::endl;
+    std::cout << normal_error(param_initial_perturbed, {0., 0., 1.}) << std::endl;
+    
+    //std::cout << "Testing jacobians" << std::endl;
+    //test_problem(param_optimize, 1);
+    
+    std::cout << "Solving Problems..." << std::endl;
+    //test_problem(param_optimize, 0, 2);
+    
+    ProblemParameters param_optimize_lvl{param_initial};
+    solve_problem(param_optimize_lvl, 1, 2, 10);
+    ProblemParameters param_optimize{param_optimize_lvl};
+    solve_problem(param_optimize, 0, 2, 10);
+    
+    ProblemParameters param_optimize_single{param_initial};
+    
+    //solve_problem(param_optimize_single, 0, 2, 20);
+    /*
+    plt::figure(1);
+    plot_patches(param_initial, 500);
+    plt::figure(2);
+    plot_patches(param_optimize, 500);
+    plt::show();
+    */
+    std::cout << "Solving perturbed problem... " << std::endl;
+    ProblemParameters param_optimize_perturbed_lvl{param_initial_perturbed};
+    //solve_problem(param_optimize_perturbed_lvl, 1, 2, 10);
+    ProblemParameters param_optimize_perturbed{param_optimize_perturbed_lvl};
+    //solve_problem(param_optimize_perturbed, 0, 2, 10);
+    
+    ProblemParameters param_optimize_perturbed_single{param_initial_perturbed};
+    //solve_problem(param_optimize_perturbed_single, 0, 2, 20);
+    
+    std::vector<double> bins_z{0.00001, 0.00005, 0.0001, 0.0005, 0.001, 0.005};
+    std::vector<long> histogram_z_initial = z_error_histogram(param_initial, -0.25, bins_z);
+    std::vector<long> histogram_z_optimize = z_error_histogram(param_optimize, -0.25, bins_z);
+    std::vector<long> histogram_z_optimize_fine = z_error_histogram(param_optimize_single, -0.25, bins_z);
+    print_std_vector(histogram_z_initial);
+    print_std_vector(histogram_z_optimize);
+    print_std_vector(histogram_z_optimize_fine);
+    
+    std::vector<double> bins_n{0.001, 0.01, 0.1};
+    std::vector<long> histogram_n_initial = normal_error_histogram(param_initial, {0., 0., 1.}, bins_n);
+    std::vector<long> histogram_n_optimize = normal_error_histogram(param_optimize, {0., 0., 1.}, bins_n);
+    std::vector<long> histogram_n_optimize_fine = normal_error_histogram(param_optimize_single, {0., 0., 1.}, bins_n);
+    print_std_vector(histogram_n_initial);
+    print_std_vector(histogram_n_optimize);
+    print_std_vector(histogram_n_optimize_fine);
+    
+    std::vector<long> histogram_n_perturbed = normal_error_histogram(param_initial_perturbed, {0., 0., 1.}, bins_n);
+    std::vector<long> histogram_n_perturbed_opt = normal_error_histogram(param_optimize_perturbed, {0., 0., 1.}, bins_n);
+    print_std_vector(histogram_n_perturbed);
+    print_std_vector(histogram_n_perturbed_opt);
+    
+    //std::cout << param_initial.get_points()[0].transpose() << std::endl;
+    //std::cout << param_initial.get_normals()[0].transpose() << std::endl;
+    for (int i = 0; i < 200; i++) {
+        if (i % 100 != 0) continue;
+        //if (normal_error(param_optimize_perturbed.get_normal(i), {0., 0., 1.}) < 0.1) continue;
+        //if ((param_optimize.get_point(i) - param_initial.get_point(i)).norm() < 0.005) continue;
+        plt::figure(1);
+        plot_patches(param_initial, i);
+        //plt::figure(2);
+        //plot_patches(param_optimize_lvl, i);
+        plt::figure(3);
+        plot_patches(param_optimize, i);
+        plt::figure(4);
+        plot_patches(param_initial_perturbed, i);
+        //plt::figure(5);
+        //plot_patches(param_optimize_perturbed_lvl, i);
+        plt::figure(6);
+        plot_patches(param_optimize_perturbed, i);
+        plt::show();
+    }
     
     std::vector<Eigen::Vector3d> ps;
     std::vector<Eigen::Vector3d> ns;
+    write_points_normals(output_path+"points_normals_complete.txt", 
+                         ps=param_all.get_points(), ns=param_all.get_normals());
+    
     write_points_normals(output_path+"points_normals_initial.txt", 
                          ps=param_initial.get_points(), ns=param_initial.get_normals());
+    std::cout << param_initial.poses[param_initial.source_views[0]] * ps[0] << std::endl;
+    std::cout << unproject_camera(param_initial.pixels[0], param_initial.planes[0], RadialCamera<double>(param_initial.camera_param)) << std::endl;
+    
     write_points_normals(output_path+"points_normals_perturbed_initial.txt", 
                          ps=param_initial_perturbed.get_points(), ns=param_initial_perturbed.get_normals());
+    std::cout << param_initial_perturbed.poses[param_initial_perturbed.source_views[0]] * ps[0] << std::endl;
+    std::cout << unproject_camera(param_initial_perturbed.pixels[0], param_initial_perturbed.planes[0], 
+                                  RadialCamera<double>(param_initial_perturbed.camera_param)) << std::endl;
+    
     write_points_normals(output_path+"points_normals_optimized.txt", 
                          ps=param_optimize.get_points(), ns=param_optimize.get_normals());
+    std::cout << param_optimize.poses[param_optimize.source_views[0]] * ps[0] << std::endl;
+    std::cout << unproject_camera(param_optimize.pixels[0], param_optimize.planes[0], RadialCamera<double>(param_optimize.camera_param)) << std::endl;
+    
     write_points_normals(output_path+"points_normals_perturbed_optimized.txt", 
                          ps=param_optimize_perturbed.get_points(), ns=param_optimize_perturbed.get_normals());
+    std::cout << param_optimize_perturbed.poses[param_optimize_perturbed.source_views[0]] * ps[0] << std::endl;
+    std::cout << unproject_camera(param_optimize_perturbed.pixels[0], param_optimize_perturbed.planes[0], 
+                                  RadialCamera<double>(param_initial.camera_param)) << std::endl;
+                         
 }
 
 
