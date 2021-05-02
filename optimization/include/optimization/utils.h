18bdfc4dba1d34d435418d77bc1233fe72a74726
Alexander Gaul <gaul.alexander@googlemail.com>
2021-05-02 16:33:16 +0200

    Ceres Optimization: Function to create image pyramids

--- optimization/include/optimization/utils.h
+++ optimization/include/optimization/utils.h
@@ -52,6 +52,55 @@ Eigen::Matrix<double, 3, 16> create_grid(Eigen::Vector3d point, Eigen::Vector3d 
     return grid;
 }
 
+void create_pyramid_images(std::vector<cv::Mat> const & images_color,
+                           std::vector<std::vector<cv::Mat>> & mat_pyramids,
+                           std::vector<std::vector<ceres::Grid2D<double, 1>>> * grid_pyramids = nullptr,
+                           std::vector<std::vector<ceres::BiCubicInterpolator<ceres::Grid2D<double, 1>>>> *  interpolator_pyramids = nullptr,
+                           std::vector<std::vector<basalt::Image<double>>> * basalt_pyramids = nullptr) {
+    for (cv::Mat image : images_color) {
+        cv::Mat im_1;
+        cv::Mat im_2;
+        
+        cv::Mat mat_color;
+        cv::cvtColor(image, mat_color, cv::ColorConversionCodes::COLOR_BGR2GRAY);
+        mat_color.convertTo(im_1, CV_64F);
+
+        cv::Mat mat_blur;
+        cv::Mat mat_clone;
+        
+        cv::GaussianBlur(im_1, mat_blur, cv::Size(3, 3), 0);
+        cv::resize(mat_blur, mat_clone, cv::Size(mat_blur.size[1]/ 2, mat_blur.size[0] / 2), 0, 0, cv::INTER_NEAREST);
+        im_2 = mat_clone.clone();
+        //cv::imshow("scale", mat2 / 255.);
+        //cv::waitKey();
+        //cv::pyrDown(mat1, mat2); //cv::BORDER_REPLICATE
+        
+        im_1 /= 255.;
+        im_2 /= 255.;
+        mat_pyramids.push_back({im_1, im_2});
+        
+        if (grid_pyramids) {
+            grid_pyramids->push_back(
+                {ceres::Grid2D<double, 1>((double*)(mat_pyramids.back()[0].data), 
+                                                    0, mat_pyramids.back()[0].rows, 0, mat_pyramids.back()[0].cols),
+                 ceres::Grid2D<double, 1>((double*)(mat_pyramids.back()[1].data), 
+                                                    0, mat_pyramids.back()[1].rows, 0, mat_pyramids.back()[1].cols)});
+            if (interpolator_pyramids) {
+                interpolator_pyramids->push_back(
+                    {ceres::BiCubicInterpolator<ceres::Grid2D<double, 1>>(grid_pyramids->back()[0]),
+                     ceres::BiCubicInterpolator<ceres::Grid2D<double, 1>>(grid_pyramids->back()[1])});
+            }
+        }
+        if (basalt_pyramids) {
+            basalt_pyramids->push_back(
+                {basalt::Image<double>((double*)mat_pyramids.back()[0].data, mat_pyramids.back()[0].cols, mat_pyramids.back()[0].rows, 8*mat_pyramids.back()[0].cols),
+                 basalt::Image<double>((double*)mat_pyramids.back()[1].data, mat_pyramids.back()[1].cols, mat_pyramids.back()[1].rows, 8*mat_pyramids.back()[1].cols)});
+        }
+    }
+    
+}
+
+// TODO: move into visuializationn utils
 void get_camera_symbol(Sophus::SE3d pose, RadialCamera<double> camera,
                        std::vector<Eigen::Vector3d>& points, 
                        std::vector<Eigen::Vector2i>& lines) {
